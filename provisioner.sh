81bdd51a810129dc09ac4f590c05ae30daa0a9b0
Nilanjan Roy <nilanjan.roy@nosto.com>
2016-03-12 21:45:34 +0200

    Added copyright

--- provisioner.sh
+++ provisioner.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright:: 2016, Nilanjan Roy (<nilu.besu@gmail.com>)
+# Authors:
+#       Nilanjan Roy <nilu.besu@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 function initialize() {
   sudo apt-get update -y
@@ -14,8 +30,9 @@ function initialize() {
 function main() {
   echo "Initial installation begins...."
   initialize
+  echo "Provisioning $name ...."
   tar -czvf chef-solo.tar.gz cookbooks roles data_bags environments
-  sudo /usr/bin/chef-solo -E $environment -j ./dna/${role}.json -r chef-solo.tar.gz -N $name && rm -rf /tmp/chef-solo
+  sudo /usr/bin/chef-solo -E $environment -j ./dna/${role}.json -r chef-solo.tar.gz -N $name && rm -rf chef-solo.tar.gz
 }
 
 
